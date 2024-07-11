9fbc2faf05782eff4944c33f7dfd55a6d5238c36
Sakomm <sakommuri@gmail.com>
2024-07-10 20:03:28 -0400

    Trash

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
-import sqlite3 as db 
+import sqlite3 as dbc
 import os 
 
 db = "shitdb.db"
 sql = "schema.sql"
+global schema
 
 def check_db():
     return os.path.exists(db)    
@@ -10,6 +11,11 @@ def check_db():
 if not check_db():
     raise Exception("DATABASE DOES NOT EXIST -- PLEASE CHECK PATH")
     
-con = db.connect(db)
+with open(sql, 'r') as rf:
+    # Read the schema from the file
+    schema = rf.read()
 
-
+with dbc.connect(db) as conn:
+    print('Created the connection!')
+    # Execute the SQL query to create the table
+    conn.executescript(schema)
